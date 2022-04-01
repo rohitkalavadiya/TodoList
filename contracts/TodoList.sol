3073530f9903b78aca520e73ea4e040cd90b1b36
rohitkalavadiya <rohitkalavadiya9@gmail.com>
2022-04-01 16:37:48 +0530

    created product done succesfully

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,31 @@ pragma solidity >= 0.4.22 < 0.9.0 ;
 
 contract TodoList{
     uint public taskCount = 0 ; 
+
+    mapping (uint => Task) public tasks;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // event CreateTask {
+    //     uint id,
+    //     string content,
+    //     bool completed
+    // };
+
+   
+    constructor() public {
+        createTask("Rohit Demo ");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content , false);
+        //emit CreateTask
+
+    }
+
 }
